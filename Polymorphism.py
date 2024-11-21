f3943e8e7ddd5c9eb05bf1a2b864dcb7e8d9605d
Kevin Langat <langatkaykay@gmail.com>
2024-11-21 10:11:50 +0300

    Polymorphism's commit

---
+++ Polymorphism.py
@@ -0,0 +1,10 @@
+class Goat:
+    def move(self):
+        print('Walking')
+class Eagle:
+    def move(self):
+        print('Flying')
+class Shark:
+    def move(self):
+        print('swimming')
+
